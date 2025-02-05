b4a4082f7d8dd328a1e4349c3193b91043126d36
elitehaad <elitehaad@icloud.com>
2025-02-04 23:12:48 -0500

    Juan takeover

---
+++ data-a01/LibraryCatalog.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+const int MAX_NAME_LENGTH = 100;
+
+typedef struct Book {
+    int id;
+    char title[MAX_NAME_LENGTH];
+    char author[MAX_NAME_LENGTH];
+    int publication_year;
+    struct Book* next;
+} Book;
+
+void addBook(Book** head, int id, const char* title, const char* author, int publication_year);
+void viewBooks(Book* head);
+void cleanBuffer();
+//void viewBooks(Book* head);
+//void updateBook(Book* head, int id);
+//void deleteBook(Book** head, int id);
+//void searchBooks(Book* head, const char* title);
