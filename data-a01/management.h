dd6f0aca4fa7950b75cdb807dde284c9991f76a3
jmonterocortes9649 <jmonterocortes9649@conestogac.on.ca>
2025-02-04 22:35:23 -0500

    viewBooks done

--- data-a01/management.h
+++ data-a01/management.h
@@ -1,12 +1,23 @@
 #pragma once
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+const int MAX_NAME_LENGTH = 100;
+
 typedef struct Book {
     int id;
-    char title[100];
-    char author[100];
+    char title[MAX_NAME_LENGTH];
+    char author[MAX_NAME_LENGTH];
     int publication_year;
     struct Book* next;
 } Book;
+
 void addBook(Book** head, int id, const char* title, const char* author, int publication_year);
+void viewBooks(Book* head);
+void cleanBuffer();
 //void viewBooks(Book* head);
 //void updateBook(Book* head, int id);
 //void deleteBook(Book** head, int id);
